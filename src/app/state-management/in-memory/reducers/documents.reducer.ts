44083e249e6e3ca5d71a6833deaf8bace73a0f28
Ben McGraw <ben.mcgraw@gmail.com>
2023-12-14 13:59:51 -0800

    documents reducer

---
+++ src/app/state-management/in-memory/reducers/documents.reducer.ts
@@ -0,0 +1,31 @@
+import {
+  createReducer,
+  Reducer /*GenericAction, Reducer*/,
+} from '../../../../shared/reducer/reducer.inc';
+
+interface DocumentFocusAction {
+  type: 'DOC_FOCUS';
+  document_id: string;
+}
+
+export type DocumentsAction =
+    | DocumentFocusAction;
+
+export interface DocumentsState {
+    activeDoc: string;
+}
+
+const initialDocumentsState: DocumentsState = {
+    activeDoc: ""
+};
+
+const documentsReducer: Reducer<DocumentsState, DocumentsAction> = createReducer(
+  initialDocumentsState,
+  {
+    DOC_FOCUS: (state, action: DocumentFocusAction) => {
+      return {...state, activeDoc: action.document_id };
+    }
+  },
+);
+
+export {initialDocumentsState, documentsReducer};
